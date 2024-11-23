5dd65b266ffe36090e57f7475ce3e359b9e842fa
Rifki Muhammad <rifmudth@gmail.com>
2024-11-23 14:16:30 +0800

    Test 1

---
+++ Dashboard/Dashboard.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib as mlt
+import seaborn as sns
+import streamlit as st
+
+# Set style seaborn
+sns.set(style='dark')
+
+# Menyiapkan data day_df
+day_df = pd.read_csv("C:\Users\USER\Desktop\Submission\Data\bike_day.csv")
+day_df.head()
+
+# Menghapus kolom yang tidak diperlukan
+drop_col = ['windspeed']
+
+for i in day_df.columns:
+  if i in drop_col:
+    day_df.drop(labels=i, axis=1, inplace=True)
