d98df1d22e8793a9188998f88a89cb1481cdfdea
Rifki Muhammad <rifmudth@gmail.com>
2024-11-30 11:08:52 +0800

    cek1

--- Dashboard/dashboard1.py
+++ Dashboard/dashboard1.py
@@ -242,6 +242,8 @@ st.pyplot(fig)
 st.markdown("---")
 st.header('5. Weekday, Workingday, and Holiday Rentals')
 
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(15,22))
+
 colors1=["tab:orange", "tab:red"]
 colors2=["tab:orange", "tab:red"]
 colors3=["tab:red", "tab:pink", "tab:blue", "tab:green", "tab:purple", "tab:brown", "tab:orange"]
